17d11192a8cdac852906197e82bf9dd6a547ea42
Daniel Díaz <diaz_carrete@yahoo.com>
2022-08-15 18:01:06 +0200

    some comments

--- default.nix
+++ default.nix
@@ -1,2 +1,4 @@
+# remember to point nixpkgs to the right fork and the right branch
+# https://github.com/danidiaz/nixpkgs/tree/haskell_avoid_recomp_experiment 
 { nixpkgs ? import /tmp/nixpkgs {} } :
 nixpkgs.haskellPackages.callPackage ./myderivation.nix {}
